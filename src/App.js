c2b24da7aa4601f0fdb015c01f382ca6c3d867b5
Joel <joelmusicman@gmail.com>
2019-10-09 20:47:39 -0500

    Wrote counter class component'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {
   BrowserRouter as Router, Route, Switch, Link,
 } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
---
+++ src/Counter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+class Counter extends React.Component {
+
+  state = {
+    count: 0
+  }
+
+
+  render() {
+    const count = this.state
+    return (
+      <div>
+        <button>
+          {count}
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Counter
